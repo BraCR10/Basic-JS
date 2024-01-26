0ddfa24ab9c446811f1d7a3dc0d999735c1b43d9
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-01-25 19:09:34 -0600

    Finalizacion de la parte de tipos

---
+++ 01-Types/08-funtions.js
@@ -0,0 +1,12 @@
+//Funcion sin retorno, osea que se debe poner dentro de la funcion el console.log() si usted quiere mostrar algo 
+function greet (   ){
+    console.log('Hello world')
+}
+
+greet (   )
+
+//Funcion con retorno, significa que la funcion devuelve un dato
+function sum(){
+    return 1+1
+}
+console.log(sum())
---
+++ 01-Types/09-arguments.js
@@ -0,0 +1,5 @@
+//Funcion con retorno y argumentos 
+function sum(num1,num2){
+    return num1+num2
+}
+console.log(sum(9,8))
